e45faf887238409f099ecf7f4067c51a90e72a1b
Adrien GIVRY <contact@adrien-givry.com>
2026-01-31 17:10:55 -0500

    Updated tab settings

--- init.lua
+++ init.lua
@@ -104,6 +104,13 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
+-- Set identations to tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 

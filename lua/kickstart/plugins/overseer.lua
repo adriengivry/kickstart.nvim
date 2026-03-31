3d21dd1b0acf19dc65e0cc9036f0a93eed1e963c
Adrien GIVRY <contact@adrien-givry.com>
2026-03-31 16:56:23 -0400

    Added <leader>o to toggle overseer

--- lua/kickstart/plugins/overseer.lua
+++ lua/kickstart/plugins/overseer.lua
@@ -2,5 +2,6 @@ return {
   'stevearc/overseer.nvim',
   ---@module 'overseer'
   ---@type overseer.SetupOpts
+  config = function() vim.keymap.set('n', '<leader>o', '<cmd>OverseerToggle<CR>') end,
   opts = {},
 }

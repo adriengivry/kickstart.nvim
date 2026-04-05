721c404917aa56cb5f3eb7ff0edee75da90cfd3b
Adrien GIVRY <contact@adrien-givry.com>
2026-04-05 12:32:31 -0400

    Updated overseer keybinds

--- lua/kickstart/plugins/overseer.lua
+++ lua/kickstart/plugins/overseer.lua
@@ -3,9 +3,9 @@ return {
   ---@module 'overseer'
   ---@type overseer.SetupOpts
   config = function()
-    vim.keymap.set('n', '<leader>ot', '<cmd>OverseerToggle<CR>')
-    vim.keymap.set('n', '<leader>or', '<cmd>OverseerRun<CR>')
-    vim.keymap.set('n', '<leader>os', '<cmd>OverseerShell<CR>')
+    vim.keymap.set('n', '<leader>o', '<cmd>OverseerToggle<CR>')
+    vim.keymap.set('n', '<F6>', '<cmd>OverseerRun<CR>')
+    vim.keymap.set('n', '<C-/>', '<cmd>OverseerShell<CR>')
   end,
   opts = {},
 }

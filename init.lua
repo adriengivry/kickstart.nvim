0a4d6337cd780f1783bf3a775367b367e83ddc7d
Adrien GIVRY <contact@adrien-givry.com>
2026-04-22 18:08:23 -0400

    Added grug-far for search and replace

--- init.lua
+++ init.lua
@@ -164,6 +164,7 @@ require('lazy').setup({
   require 'kickstart.plugins.nvim-dap',
   require 'kickstart.plugins.nvim-dap-ui',
   require 'kickstart.plugins.nvim-dap-lldb',
+  require 'kickstart.plugins.grug-far',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
